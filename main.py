6e34b0fdc15f7c991bed59d2fbe96e78bf2a6f2f
sinonymmm <105551562+sinonymmm@users.noreply.github.com>
2024-11-16 13:58:34 +0700

    main.py

--- main.py
+++ main.py
@@ -154,10 +154,15 @@ def register():
         if register_user(username, password):
             st.success("Akun berhasil dibuat")
             st.info("Silakan login dengan akun Anda")
+
+            # Set session state logged_in ke False karena belum login
+            st.session_state.logged_in = False
+
             # Memanggil rerun untuk langsung menuju halaman login setelah registrasi
             st.experimental_rerun()  # Refresh halaman untuk menampilkan halaman login
         else:
             st.warning("Username sudah terdaftar")
+
 
 
 # Inisialisasi state
